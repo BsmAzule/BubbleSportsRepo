00b0efcd596d3afa1c0ec1ade4421fdc9b7e8f8b
Arthur G <arthurgan29@gmail.com>
2025-01-25 16:49:34 -0800

    Wiimote input

--- BubbleSports/Assets/Scripts/Services/GameInput.cs
+++ BubbleSports/Assets/Scripts/Services/GameInput.cs
@@ -10,11 +10,15 @@ namespace Input
         [SerializeField]
         private InputEvent _inputEvent;
 
+        [SerializeField]
+        private InputEvent _inputEvent2;
+
         private void Update()
         {
             if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
             {
                 _inputEvent.Raise(new InputData { Value = 1 });
+                _inputEvent2.Raise(new InputData { Value = 0.8f });
             }
         }
     }
---
+++ BubbleSports/Assets/Scripts/Wiimote/WiimoteInput.cs
@@ -0,0 +1,78 @@
+using Events.Core;
+using UnityEngine;
+using WiimoteApi;
+
+public class WiimoteInput : MonoBehaviour
+{
+    [Header("Config")]
+
+    [SerializeField]
+    private Vector2 _accelRange;
+
+    [Header("Events (Out)")]
+
+    [SerializeField]
+    private InputEvent _AInputEvent;
+
+    [SerializeField]
+    private InputEvent _accelInputEvent;
+
+    private Wiimote _wiimote;
+
+    private void Awake()
+    {
+        InitWiimotes();
+    }
+
+    private void Update()
+    {
+        int ret;
+        do
+        {
+            ret = _wiimote.ReadWiimoteData();
+            if (_wiimote.Button.a)
+            {
+                _AInputEvent.Raise(new InputData { Value = 1 });
+            }
+
+            float[] accelData = _wiimote.Accel.GetCalibratedAccelData();
+            var accelVector = new Vector3(accelData[0], accelData[1], accelData[2]);
+            // Debug.Log($"Accel: {accelVector}, magnitude: {accelVector.magnitude}");
+
+            float accelNormalized = Mathf.InverseLerp(_accelRange.x, _accelRange.y, accelVector.magnitude);
+
+            if (accelNormalized > 0f)
+            {
+                _accelInputEvent.Raise(new InputData { Value = accelNormalized });
+            }
+        } while (ret > 0);
+    }
+
+    private void OnDestroy()
+    {
+        FinishedWithWiimotes();
+    }
+
+    private void InitWiimotes()
+    {
+        WiimoteManager.FindWiimotes(); // Poll native bluetooth drivers to find Wiimotes
+        Debug.Log($"Found {WiimoteManager.Wiimotes.Count} wiimotes");
+        foreach (Wiimote remote in WiimoteManager.Wiimotes)
+        {
+            _wiimote = remote;
+            _wiimote.Accel.CalibrateAccel(AccelCalibrationStep.A_BUTTON_UP);
+            remote.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL);
+            remote.SendPlayerLED(true, false, false, false);
+            break;
+            // Do stuff.
+        }
+    }
+
+    private void FinishedWithWiimotes()
+    {
+        foreach (Wiimote remote in WiimoteManager.Wiimotes)
+        {
+            WiimoteManager.Cleanup(remote);
+        }
+    }
+}
